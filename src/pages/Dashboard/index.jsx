32e38a4a23cea7b361f65960eba0261a2413ebc0
Lucio Noleto <luciolnoleto@gmail.com>
2022-10-17 19:13:24 -0300

    Atualização de Front-end no Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,26 +1,23 @@
 import {Icon, Card, DateSelect  } from '~/components';
 
-import { useState , useEffect} from 'react';
+import { useEffect, useState } from 'react';
 import {useLocalStorage, useAsyncFn} from 'react-use'
-import {  useParams, useNavigate } from 'react-router-dom'
-
+import { useParams, Navigate } from 'react-router-dom'
 import axios from 'axios'
+
 import {format, formatISO} from 'date-fns'
-
-
 
 
 export const Dashboard = () => {
     const params = useParams()
-    const navigate = useNavigate()
     const [currentDate, setDate] = useState(formatISO(new Date(2022,10,20)))
-    const [auth, setAuth] = useLocalStorage('auth',{})
-    
+    const [auth] = useLocalStorage('auth', {})
+
     const [{value: user, loading, error}, fetchHunches] = useAsyncFn(async () => {
         const res = await axios({
             method: 'get',
             baseURL: import.meta.env.VITE_API_URL,
-            url: `/${user.username}`,
+            url: `/${params.username}`,
         })
         
         const hunches = res.data.hunches.reduce((acc, hunch)=> {
@@ -38,31 +35,30 @@ export const Dashboard = () => {
     const [games,fetchGames] = useAsyncFn(async(params)=>{
         const res = await axios({
             method: 'get',
-            baseURL: import.meta.env.VITE_API_URL,
+            baseURL: 'http://localhost:3000',
             url: '/games',
             params
         })
         return res.data
     })
-
+    
     useEffect(()=>{
-        fetchHunches()  
-    },[])
+        fetchHunches()
+    },[currentDate])
     
     useEffect(()=>{
         fetchGames({gameTime: currentDate})
     },[currentDate])
 
-    const isLoading = games.loading || loading
-    const isError = games.error 
-    const isDone = !isLoading && !isError
-
-    
 
     if (!auth.user?.id){
         return <Navigate to='/' replace={true} />
     }
-    
+
+    const isLoading = games.loading || loading
+    const isError = games.error 
+    const isDone = !isLoading && !isError
+
     return (
         <>
             <header className="bg-red-500 text-white p-4">
@@ -72,41 +68,36 @@ export const Dashboard = () => {
                         <Icon name='profile' color="white" className="w-10" />
                     </a>
                 </div>
+
             </header>
             <main className='space-y-6'>
                 
-                <section id='header' className='bg-red-500 text-white p-4 space-y-2'>
-                    <div className='container max-w-3xl'>
-                        <a href='/dashboard'>
-                            <Icon name="back" className="w-10" />
-                        </a>
-                        <h3 className='text-2xl font-bold py-6'>{user?.name}</h3>
+                <section id='header' className='bg-red-500 text-white p-4'>
+                    <div className='container max-w-3xl space-y-2'>
+                        <span>Olá {auth.user.name}</span>
+                        <h3 className='text-2xl font-bold py-6'>Qual o seu palpite?</h3>
                     </div>
                 </section>
                     
                 <section id='content' className='container max-w-3xl p-4 space-y-4'>
                     
-                    <h2 className='text-xl text-red-700 font-bold'>Seus palpites</h2>
-
                     <DateSelect currentDate={currentDate} onChange={setDate}/>
                     
                     <div className='space-y-4'>
                         {isLoading && 'Carregando jogos...'}
                         {isError && 'Ops! Algo deu errado.'}
                         {isDone && games.value?.map(game => 
-                            <Card
-                                key={game.id}
-                                gameId={game.id}
-                                homeTeam={game.homeTeam}
-                                awayTeam={game.awayTeam}
-                                gameTime={format(new Date(game.gameTime), 'H:mm')}
-                                homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ''}
-                                awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ''}
-                                
-                            /> 
-                        )}
+                        <Card
+                            key={game.id}
+                            gameId={game.id}
+                            homeTeam={game.homeTeam}
+                            awayTeam={game.awayTeam}
+                            gameTime={format(new Date(game.gameTime), 'H:mm')}
+                            homeTeamScore={user?.hunches?.[game.id]?.homeTeamScore || ''}
+                            awayTeamScore={user?.hunches?.[game.id]?.awayTeamScore || ''}
+                        /> )}
+                                               
                     </div>
-                    
                 </section>
             
             </main>
